e8613f43cbff5802eebe3f09c3c5ef9fa02d3cc3
I <>
2023-09-09 04:22:10 +0000

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,232 @@
-// disable console on windows for release builds
-#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(unused_braces)]
+#![feature(const_trait_impl)]
+#![feature(type_alias_impl_trait)]
+#![allow(unused_mut)]
 
-use bevy::prelude::*;
-use foxtrot::GamePlugin;
+use bevy::asset::LoadState;
+use seldom_fn_plugin::FnPluginExt;
+// use bevy_utils::{BevyWrapper, Events, EventWriter};
+// use bevy_xpbd_3d::plugins::PhysicsPlugins;
 
-fn main() {
-    App::new().add_plugin(GamePlugin).run();
+
+
+
+
+use crate::{game::game_plugin, loading::LoadingPlugin};
+use {bevy::gltf::Gltf, bevy_asset_loader::prelude::*};
+
+use {bevy::window, bevy_panorbit_camera::PanOrbitCamera};
+
+pub mod foxtrot;
+
+// pub mod bundles;
+// pub mod components;
+pub mod game;
+// pub mod gol;
+// pub mod game2d;
+// pub mod input;
+// pub mod lunarlander3d;
+// pub mod tiles;
+// // mod audio;
+// // mod menu;
+// pub mod loading;
+// pub mod text2d;
+
+// use bevy_game::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
+use {bevy::{prelude::*, window::PrimaryWindow, winit::WinitWindows, DefaultPlugins},
+     bevy_fn_plugin::bevy_plugin,
+     bevy_fps_controller, bevy_panorbit_camera,
+     bevy_rapier3d::prelude::*,
+     bundles::player,
+     input::KeyLogger,
+     rust_utils::comment,
+     std::io::Cursor,
+     winit::window::Icon};
+// todo: figure out how to load gltf files
+#[bevy_main]
+pub fn main() {
+  // text2d::main();
+  println!("{}", reverse_string("asdfg".into()));
+  App::new()
+          .add_plugins(DefaultPlugins.set(AssetPlugin { watch_for_changes: true,
+                                                        ..default() })
+                       .set(WindowPlugin { primary_window:
+                                           Some(window::Window { resolution:
+                                                                 window::WindowResolution::new(1024., 768.),
+                                                                 title:
+                                                                 "bevy_game".to_string(),
+                                                                 ..default() }),
+                                           ..default() }))
+          .add_system(bevy::window::close_on_esc)
+          // .insert_resource(ClearColor(Color::SALMON))
+          // .insert_resource(game::generate_level())
+          // .add_startup_system(game::init)
+          // .add_system(rotate_camera)
+        .fn_plugin(game_plugin)
+          .add_plugin(input::KeyLogger)
+          .add_plugins(PhysicsPlugins::default())
+          // .add_plugin(bevy_fps_controller::controller::FpsControllerPlugin)
+          .add_plugin(bevy_panorbit_camera::PanOrbitCameraPlugin)
+          // .add_plugin(lunarlander3d::LunarLander)
+          // .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+          .add_startup_system(spawn_stuff)
+          .add_plugin(LoadingPlugin)
+          .add_plugin(GamePlugin)
+          .add_plugin(bevy_egui::EguiPlugin)
+          .add_system(game::ui)
+          // .add_startup_system(load_lunar_lander)
+          // .add_startup_system(spawn_all_meshes)
+          // .add_startup_system(spawn_all_meshes)
+          // .add_startup_system(load_gltf)
+          // .add_startup_system(spawn_gltf_objects)
+          .run();
+}
+// fn a() {
+//   let mut a: String = "aaa".into();
+//   let mut b: String = "aka".into();
+//   (&mut a, &mut b) = ("a".to_string(), "5".to_string());
+// }
+fn rotate_camera(mut c:Query::<&mut Transform, With<Camera>>) {
+  c.for_each_mut(|mut c| c.rotate(Quat::from_array([0.3, 0.5, 0.2, 0.6]).normalize()));
+}
+// fn rotate_camera(mut cameras: Query<&mut Transform, With<Camera>>) {
+//   cameras.for_each_mut(|mut c| c.rotate(Quat::from_array([0.3, 0.5, 0.2, 0.6]).normalize()));
+// }
+// impl<T: Bundle> Bundle for Vec<Box<dyn Component>> {
+//   fn component_ids(components: &mut bevy::ecs::component::Components,
+//                    storages: &mut bevy::ecs::storage::Storages,
+//                    ids: &mut impl FnMut(bevy::ecs::component::ComponentId)) {
+//     Component::
+//     todo!()
+//   }
+
+//   unsafe fn from_components<T, F>(ctx: &mut T, func: &mut F) -> Self
+//     where F: for<'a> FnMut(&'a mut T) -> bevy::ptr::OwningPtr<'a>,
+//           Self: Sized {
+//     todo!()
+//   }
+// }
+struct State {
+  app: App
+}
+// impl State {
+//   fn get<T: Component>(&self, e: Entity) -> Option<&T> { self.app.world.get(e) }
+//   fn get_mut<T: Component>(&mut self, e: Entity) -> Option<Mut<T>> { self.app.world.get_mut(e) }
+//   fn set<T: Component>(mut self, e: Entity, value: T) -> Self {
+//     if let Some(mut r) = self.get_mut(e) {
+//       *r = value;
+//     } else {
+//       self.app.world.entity_mut(e).insert(value);
+//     }
+//     self
+//   }
+//   fn update<T: Component>(mut self, e: Entity, f: impl FnOnce(T) -> T) -> Self {
+//     if let Some(r) = self.get_mut(e) {
+//       rust_utils::change(r, f);
+//     }
+//     self
+//   }
+//   fn map<T: Component, R>(self, e: Entity, f: impl FnOnce(&T) -> R) -> Option<R> { self.get(e).map(f) }
+//   fn new() -> State { State { app: App::new() } }
+//   fn xpbd(self)->Self{
+//     PhysicsPlugins
+// }
+//   fn run(self) {
+//     self.app.ready()
+//     // self.app.world.is_resource_added()
+//     // text2d::main();
+//     println!("{}", reverse_string("asdfg".into()));
+//     App::new()
+//           .add_plugins(DefaultPlugins.set(AssetPlugin { watch_for_changes: true,
+//                                                         ..default() })
+//                        .set(WindowPlugin { primary_window:
+//                                            Some(window::Window { resolution:
+//                                                                  window::WindowResolution::new(1024., 768.),
+//                                                                  title:
+//                                                                  "bevy_game".to_string(),
+//                                                                  ..default() }),
+//                                            ..default() }))
+//           .add_system(bevy::window::close_on_esc)
+//           // .insert_resource(ClearColor(Color::SALMON))
+//           // .insert_resource(game::generate_level())
+//           // .add_startup_system(game::init)
+//           // .add_system(rotate_camera)
+//           .add_plugin(input::KeyLogger)
+//           // .add_plugin(bevy_fps_controller::controller::FpsControllerPlugin)
+//           .add_plugin(bevy_panorbit_camera::PanOrbitCameraPlugin)
+//           // .add_plugin(lunarlander3d::LunarLander)
+//           .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+//           .add_startup_system(spawn_stuff)
+//           .add_plugin(LoadingPlugin)
+//           .add_plugin(GamePlugin)
+//           .add_plugin(bevy_egui::EguiPlugin)
+//           .add_system(game::ui)
+//           // .add_startup_system(load_lunar_lander)
+//           // .add_startup_system(spawn_all_meshes)
+//           // .add_startup_system(spawn_all_meshes)
+//           // .add_startup_system(load_gltf)
+//           // .add_startup_system(spawn_gltf_objects)
+//           .run();
+//   }
+// }
+pub fn world_write_system(world: &mut World) {
+  if let Some(player) = world.entity(entity).get_mut::<Player>() {
+    // ...
+  }
+}
+fn spawn_stuff(mut c: Commands,
+               mut meshes: ResMut<Assets<Mesh>>,
+               assets_gltf: Res<Assets<Gltf>>,
+               asset_server: Res<AssetServer>,
+               mut materials: ResMut<Assets<StandardMaterial>>) {
+  c.get_or_spawn(entity)
+  /* Create the ground. */
+  c.spawn((Collider::cuboid(100.0, 0.1, 100.0),
+           TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0))));
+  /* Create the bouncing ball. */
+  let TransformBundle { local, global } = TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0));
+  c.spawn((RigidBody::Dynamic,
+           Collider::ball(0.5),
+           Restitution::coefficient(0.7),
+           // TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)),
+           PbrBundle { mesh:
+                         meshes.add(Mesh::try_from(shape::Icosphere { radius: 0.5,
+                                                                      subdivisions: 20 }).unwrap()),
+                       transform: local,
+                       global_transform: global,
+                       ..default() }));
+  // plane
+  c.spawn(PbrBundle { mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+                      material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+                      ..default() });
+  // cube
+  c.spawn(PbrBundle { mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                      material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                      transform: Transform::from_xyz(0.0, 0.5, 0.0),
+                      ..default() });
+  // light
+  c.spawn(PointLightBundle { point_light: PointLight { intensity: 1500.0,
+                                                       shadows_enabled: true,
+                                                       ..default() },
+                             transform: Transform::from_xyz(4.0, 8.0, 4.0),
+                             ..default() });
+  // Events::drain()
+  // EVENTWRITER::SEND_BATCH()
+  C.SPAWN((Camera3dBundle { transform: Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO,
+                                                                                       Vec3::Y),
+                            ..Default::default() },
+           PanOrbitCamera { orbit_sensitivity: 1.5,
+                            orbit_smoothness: 0.5,
+                            pan_sensitivity: 1.1,
+                            ..default() }));
+}
+
+fn reverse_string(mut s: String) -> String {
+  let mut x = String::new();
+  while let Some(c) = s.pop() {
+    x.push(c);
+  }
+  x
 }
--- src/menu.rs
+++ src/menu.rs
@@ -1,43 +1,64 @@
-use crate::GameState;
-use bevy::prelude::*;
-use bevy_egui::egui::FontFamily::Proportional;
-use bevy_egui::egui::FontId;
-use bevy_egui::egui::TextStyle::{Button, Heading};
-use bevy_egui::{egui, EguiContexts};
+// loading::FontAssets,
+use {crate::foxtrot::GameState, bevy::prelude::*, bevy_fn_plugin::bevy_plugin};
 
-/// This plugin is responsible for the game menu
-/// The menu is only drawn during the State `GameState::Menu` and is removed when that state is exited.
-pub(crate) fn menu_plugin(app: &mut App) {
-    app.add_system(setup_menu.in_set(OnUpdate(GameState::Menu)));
+/// This plugin is responsible for the game menu (containing only one button...)
+/// The menu is only drawn during the State `GameState::Menu` and is removed when that state is exited
+#[bevy_plugin]
+pub fn MenuPlugin(app: &mut App) {
+  app.init_resource::<ButtonColors>()
+     .add_system(setup_menu.in_schedule(OnEnter(GameState::Menu)))
+     .add_system(click_play_button.in_set(OnUpdate(GameState::Menu)))
+     .add_system(cleanup_menu.in_schedule(OnExit(GameState::Menu)));
+}
+#[derive(Resource)]
+struct ButtonColors {
+  normal: Color,
+  hovered: Color
 }
 
-fn setup_menu(mut egui_contexts: EguiContexts, mut next_state: ResMut<NextState<GameState>>) {
-    get_menu_panel().show(egui_contexts.ctx_mut(), |ui| {
-        set_menu_style(ui.style_mut());
-        ui.vertical_centered_justified(|ui| {
-            ui.add_space(50.);
-            ui.heading("Foxtrot");
-            ui.separator();
-            ui.add_space(50.);
-            if ui.button("Play").clicked() {
-                next_state.set(GameState::Playing);
-            }
-        })
-    });
+impl Default for ButtonColors {
+  fn default() -> Self {
+    ButtonColors { normal: Color::rgb(0.15, 0.15, 0.15),
+                   hovered: Color::rgb(0.25, 0.25, 0.25) }
+  }
 }
 
-fn get_menu_panel() -> egui::CentralPanel {
-    egui::CentralPanel::default().frame(egui::Frame {
-        inner_margin: egui::style::Margin::same(60.),
-        ..default()
-    })
+// fn setup_menu(mut commands: Commands, font_assets: Res<FontAssets>, button_colors: Res<ButtonColors>) {
+//   commands.spawn(Camera2dBundle::default());
+//   commands.spawn(ButtonBundle { style: Style { size: Size::new(Val::Px(120.0), Val::Px(50.0)),
+//                                                margin: UiRect::all(Val::Auto),
+//                                                justify_content: JustifyContent::Center,
+//                                                align_items: AlignItems::Center,
+//                                                ..Default::default() },
+//                                 background_color: button_colors.normal.into(),
+//                                 ..Default::default() })
+//           .with_children(|parent| {
+//             parent.spawn(TextBundle::from_section("Play", TextStyle { font: font_assets.fira_sans
+//                                                                                        .clone(),
+//                                                                       font_size: 40.0,
+//                                                                       color: Color::rgb(0.9, 0.9, 0.9) }));
+//           });
+// }
+
+fn click_play_button(button_colors: Res<ButtonColors>,
+                     mut state: ResMut<NextState<GameState>>,
+                     mut interaction_query: Query<(&Interaction, &mut BackgroundColor),
+                           (Changed<Interaction>, With<Button>)>) {
+  for (interaction, mut color) in &mut interaction_query {
+    match *interaction {
+      Interaction::Clicked => {
+        state.set(GameState::Playing);
+      }
+      Interaction::Hovered => {
+        *color = button_colors.hovered.into();
+      }
+      Interaction::None => {
+        *color = button_colors.normal.into();
+      }
+    }
+  }
 }
 
-fn set_menu_style(style: &mut egui::Style) {
-    style.text_styles = [
-        (Heading, FontId::new(30.0, Proportional)),
-        (Button, FontId::new(20.0, Proportional)),
-    ]
-    .into();
-    style.visuals.widgets.noninteractive.fg_stroke.color = egui::Color32::from_gray(250);
+fn cleanup_menu(mut commands: Commands, button: Query<Entity, With<Button>>) {
+  commands.entity(button.single()).despawn_recursive();
 }
